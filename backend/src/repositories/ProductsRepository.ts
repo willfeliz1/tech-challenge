d8de9643060e1ee78dcc3103ffbdf5d7a67dd0a7
willfeliz1 <william_mfelizardo@hotmail.com>
2020-11-21 16:35:31 -0300

    feat: interface de metodos | metodo para encontrar produtos existentes | metodo atualizar quantidade

--- backend/src/repositories/ProductsRepository.ts
+++ backend/src/repositories/ProductsRepository.ts
@@ -1,8 +1,12 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, In, Repository } from 'typeorm';
 import ICreateProductDTO from '../dtos/Products/ICreateProductDTO';
+import IUpdateProductsQuantityDTO from '../dtos/Products/IUpdateProductsQuantityDTO';
 import Product from '../entities/Product';
 import IProductsRepository from '../repositoriesInterface/IProductsRepository';
 
+interface IFindProducts {
+  id: string;
+}
 class ProductsRepository implements IProductsRepository {
   private ormRepository: Repository<Product>;
 
@@ -26,6 +30,24 @@ class ProductsRepository implements IProductsRepository {
     await this.ormRepository.save(product);
 
     return product;
+  }
+
+  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
+    const productIds = products.map(product => product.id);
+
+    const existentProducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    return existentProducts;
+  }
+
+  public async updateQuantity(
+    products: IUpdateProductsQuantityDTO[],
+  ): Promise<Product[]> {
+    return this.ormRepository.save(products);
   }
 }
 
--- backend/src/repositoriesInterface/IProductsRepository.ts
+++ backend/src/repositoriesInterface/IProductsRepository.ts
@@ -1,6 +1,12 @@
 import ICreateProductDTO from '../dtos/Products/ICreateProductDTO';
+import IUpdateProductsQuantityDTO from '../dtos/Products/IUpdateProductsQuantityDTO';
 import Product from '../entities/Product';
 
+interface IFindProducts {
+  id: string;
+}
 export default interface IProductsRepository {
   create(data: ICreateProductDTO): Promise<Product>;
+  findAllById(products: IFindProducts[]): Promise<Product[]>;
+  updateQuantity(products: IUpdateProductsQuantityDTO[]): Promise<Product[]>;
 }
