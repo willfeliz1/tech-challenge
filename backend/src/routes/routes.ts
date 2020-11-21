5faf8a2e4097e2da5bd4c4aa53b277e5b5eddd5f
willfeliz1 <william_mfelizardo@hotmail.com>
2020-11-21 16:37:20 -0300

    feat: rota de estoque

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productsRouter from './products.routes';
+import stocksRouter from './stocks.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/stocks', stocksRouter);
 
 export default routes;
---
+++ backend/src/routes/stocks.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import StocksController from '../controller/StocksController';
+
+const stocksRouter = Router();
+const stocksController = new StocksController();
+
+stocksRouter.post('/', stocksController.create);
+// stocksRouter.get('/', stocksController.findAll);
+
+export default stocksRouter;
