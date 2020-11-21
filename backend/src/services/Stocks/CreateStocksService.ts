ecbdab8743dcb6cb8111eee5ada54f39883b4b39
willfeliz1 <william_mfelizardo@hotmail.com>
2020-11-21 16:41:24 -0300

    feat: Service de Estoque: Verifica Produto existente, Qtde do produto e entao registra no banco

---
+++ backend/src/services/Stocks/CreateStocksService.ts
@@ -0,0 +1,83 @@
+import { inject, injectable } from 'tsyringe';
+import Stocks from '../../entities/Stocks';
+import AppError from '../../errors/AppError';
+import IProductsRepository from '../../repositoriesInterface/IProductsRepository';
+import IStocksRepository from '../../repositoriesInterface/IStocksRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  name: string;
+  products: IProduct[];
+}
+
+@injectable()
+class CreateStocksService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+
+    @inject('StocksRepository')
+    private stocksRepository: IStocksRepository,
+  ) {}
+
+  public async execute({ name, products }: IRequest): Promise<Stocks> {
+    const existentProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (!existentProducts.length) {
+      throw new AppError('Could not find any products with the given ids');
+    }
+
+    const existentProductsIds = existentProducts.map(product => product.id);
+
+    const checkInexistentProducts = products.filter(
+      product => !existentProductsIds.includes(product.id),
+    );
+
+    if (checkInexistentProducts.length) {
+      throw new AppError(
+        `Could not find product ${checkInexistentProducts[0].id} `,
+      );
+    }
+
+    const findProductsWithNoQtdAvailable = products.filter(
+      product =>
+        existentProducts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+
+    if (findProductsWithNoQtdAvailable.length) {
+      throw new AppError(
+        `Could not find product ${findProductsWithNoQtdAvailable[0].quantity} is not available for ${findProductsWithNoQtdAvailable[0].id} `,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+    }));
+
+    const orderedProductsQuantity = products.map(product => ({
+      id: product.id,
+      quantity:
+        existentProducts.filter(p => p.id === product.id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
+    const stock = await this.stocksRepository.create({
+      name,
+      products: serializedProducts,
+    });
+
+    return stock;
+  }
+}
+
+export default CreateStocksService;
