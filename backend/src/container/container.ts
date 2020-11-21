1d607e25e38e2073534c6d64381203b90fe8f183
willfeliz1 <william_mfelizardo@hotmail.com>
2020-11-21 16:42:06 -0300

    feat: instanciamento de StockRepository

--- backend/src/container/container.ts
+++ backend/src/container/container.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe';
 import ProductsRepository from '../repositories/ProductsRepository';
+import StocksRepository from '../repositories/StocksRepository';
 import IProductsRepository from '../repositoriesInterface/IProductsRepository';
+import IStocksRepository from '../repositoriesInterface/IStocksRepository';
 
 container.registerSingleton<IProductsRepository>(
   'ProductsRepository',
   ProductsRepository,
+);
+
+container.registerSingleton<IStocksRepository>(
+  'StocksRepository',
+  StocksRepository,
 );
